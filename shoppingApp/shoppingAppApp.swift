eba8340ffdba856ef913e5666c17394cd7e6e467
Semih Karahan <karahansemih@hotmail.com>
2023-04-20 22:46:28 +0300

    Initial Commit

---
+++ shoppingApp/shoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  shoppingAppApp.swift
+//  shoppingApp
+//
+//  Created by Semih Karahan on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct shoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
